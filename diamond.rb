debbdc36d41eac22452551d247dac1a24ad19dc3
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-24 14:16:10 -0600

    Implement make half functionality.

--- diamond.rb
+++ diamond.rb
@@ -4,4 +4,16 @@ class Diamond
   def self.make_alphabet(letter)
     ('a'.."#{letter}").to_a
   end
+
+  def self.make_half(letter)
+    alphabet = make_alphabet(letter)
+    max_spaces = alphabet.count
+    alphabet.map.with_index do |letter, i|
+      if i == 0
+        " " * (max_spaces - i) + letter + "\n"
+      else
+        " " * (max_spaces - i) + letter + " " * (i + i-1) + letter + "\n"
+      end
+    end
+  end
 end
